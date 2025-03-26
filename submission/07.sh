1be944aef34554f8aae9bbc4516f16815ad4a86f
adtrex <toluadekunte@gmail.com>
2025-03-27 00:23:22 +0100

    fix txn hex

--- submission/07.sh
+++ submission/07.sh
@@ -15,9 +15,20 @@ decoded_tx=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx")
 utxo_txid=$(echo "$decoded_tx" | jq -r '.txid')
 
 
-utxo_vout=$(echo "$decoded_tx" | jq -r '.vout[0].n')
+inputs="["
+num_utxos=$(echo "$decoded_tx" | jq '.vout | length')
+for (( i=0; i<num_utxos; i++ )); do
+    vout_index=$(echo "$decoded_tx" | jq -r ".vout[$i].n")
+    inputs+="{\"txid\":\"$utxo_txid\",\"vout\":$vout_index},"
+done
+inputs=${inputs%,}
+inputs+="]"
+
+# utxo_vout=$(echo "$decoded_tx" | jq -r '.vout[0].n')
+
+rawtxhex=$(bitcoin-cli -regtest createrawtransaction "$inputs" "{ \"$recipient_address\": $amount_to_send }")
 
 
-rawtxhex=$(bitcoin-cli -regtest createrawtransaction '''[ { "txid": "'$utxo_txid'", "vout": '$utxo_vout' } ]''' '''{ "'$recipient_address'": "'$amount_to_send'" }''')
+# rawtxhex=$(bitcoin-cli -regtest createrawtransaction '''[ { "txid": "'$utxo_txid'", "vout": '$utxo_vout' } ]''' '''{ "'$recipient_address'": "'$amount_to_send'" }''')
 
 echo "$rawtxhex"
