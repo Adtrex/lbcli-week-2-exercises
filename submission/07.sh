c3eb9f6640e28ffacd246d8c57854a4faf2ae901
adtrex <toluadekunte@gmail.com>
2025-03-26 23:51:49 +0100

    fix txn hex

--- submission/07.sh
+++ submission/07.sh
@@ -15,7 +15,7 @@ decoded_tx=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx")
 utxo_txid=$(echo "$decoded_tx" | jq -r '.txid')
 
 
-utxo_vout=$(echo "$decoded_tx" | jq -r '.vout[1].n')
+utxo_vout=$(echo "$decoded_tx" | jq -r '.vout | to_entries | .[0].key')
 
 
 rawtxhex=$(bitcoin-cli -regtest createrawtransaction "[ { \"txid\": \"$utxo_txid\", \"vout\": $utxo_vout } ]" "{ \"$recipient_address\": $amount_to_send }")
