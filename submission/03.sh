c6d9ec8201c40d8b06c8efb47f0714e50d0e4670
adtrex <toluadekunte@gmail.com>
2025-03-26 22:10:14 +0100

    txn value in sat

--- submission/03.sh
+++ submission/03.sh
@@ -2,16 +2,7 @@
 # Add funds to the address.
 # Return only the Address
 
-
-bitcoind -regtest -daemon
-sleep 3 
-
-
 WALLET_NAME="btrustwallet"
-EXISTING_WALLETS=$(bitcoin-cli -regtest listwallets | grep -o "$WALLET_NAME")
-if [ -z "$EXISTING_WALLETS" ]; then
-    bitcoin-cli -regtest createwallet "$WALLET_NAME"
-fi
 
 bitcoin-cli -regtest loadwallet "$WALLET_NAME"
 
