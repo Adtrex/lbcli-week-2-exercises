da380ff64f6c15d83938e79ac95e9e0ca8b4de28
adtrex <toluadekunte@gmail.com>
2025-03-27 13:39:23 +0100

    check sequence

--- submission/09.sh
+++ submission/09.sh
@@ -210,26 +210,29 @@ echo ""
 
 # STUDENT TASK: Decode the raw transaction
 # WRITE YOUR SOLUTION BELOW:
-DECODED_TX=$(bitcoin-cli -regtest decoderawtransaction $RAW_TX)
+DECODED_TX=$(bitcoin-cli  -regtest -rpcwallet=btrustwallet decoderawtransaction $RAW_TX)
 check_cmd "Transaction decoding" "DECODED_TX" "$DECODED_TX"
 
 # STUDENT TASK: Extract and verify the key components from the decoded transaction
 # WRITE YOUR SOLUTION BELOW:
 
+# extract sequence number
 sequence_number=$(echo $DECODED_TX | jq -r ".vin[]"| jq -r ".sequence" )
 
+# verify sequence number
 if [[ "$sequence_number" -gt 0 && "$sequence_number" -lt 4294967294 ]]; then
     VERIFY_RBF="true"
 else
     VERIFY_RBF="false"
 fi
-# VERIFY_RBF=$(echo "$DECODED_TX" | jq '.vin[0].sequence < 4294967295')
+
+
 check_cmd "RBF verification" "VERIFY_RBF" "$VERIFY_RBF"
 
-VERIFY_PAYMENT=$(echo "$DECODED_TX" | jq --arg addr "$PAYMENT_ADDRESS" '.vout[] | select(.scriptPubKey.addresses[0] == $addr) | .value')
+VERIFY_PAYMENT=$(echo $DECODED_TX | jq -r ".vout[0]"| jq -r ".value" )
 check_cmd "Payment verification" "VERIFY_PAYMENT" "$VERIFY_PAYMENT"
 
-VERIFY_CHANGE=$(echo "$DECODED_TX" | jq --arg addr "$CHANGE_ADDRESS" '.vout[] | select(.scriptPubKey.addresses[0] == $addr) | .value')
+VERIFY_CHANGE=$(echo $DECODED_TX | jq -r ".vout[1]"| jq -r ".value" )
 check_cmd "Change verification" "VERIFY_CHANGE" "$VERIFY_CHANGE"
 
 echo "Verification Results:"
